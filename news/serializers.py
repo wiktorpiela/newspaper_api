7f886291c6c6c30649d65d1e3ca6e765b1a5ad5c
Wiktor <wpiela9@gmail.com>
2023-03-31 17:58:42 +0200

    token auth added

--- news/serializers.py
+++ news/serializers.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.http import Http404
 from django.contrib.auth.password_validation import validate_password
+from rest_framework.validators import UniqueValidator
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,10 +14,20 @@ class ArticleSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     articles = serializers.PrimaryKeyRelatedField(many=True, queryset=Article.objects.all()) #wiele artykułów do modelu usera
+    email = serializers.EmailField(
+        required = False,
+        validators=[UniqueValidator(
+            queryset = User.objects.all(),
+            message = "A user with that email already exists."
+        )]
+    )
 
     class Meta:
         model = User
         fields = ("id", "username", "email", "password", "articles",)
+        extra_kwargs = {
+            "password": {"write_only":True},
+        }
 
     def create(self, validated_data): #requested data
         username = validated_data.get("username")   
@@ -29,11 +40,7 @@ class UserSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(e.messages)
         else:
             if email:
-                emailTaken = User.objects.filter(email=email).exists()
-                if not emailTaken:
-                    user = User.objects.create(username=username,email=email,password=password)  
-                else:
-                    raise serializers.ValidationError("This email is already taken.")
+                user = User.objects.create_user(username=username,email=email,password=password)  
             else:
-                user = User.objects.create(username=username,password=password)   
+                user = User.objects.create_user(username=username,password=password)   
             return user
