6f6cada954c9b58dc515467da4bc52653672cb31
Wiktor <wpiela9@gmail.com>
2023-06-13 19:42:12 +0200

    frontend added, js hoisting, functions

--- frontend/script.js
+++ frontend/script.js
@@ -3,12 +3,17 @@ const title = document.querySelector("h1")
 const isSuccess = true
 let articles
 
+
+
+
+//hoisitng
 if (isSuccess) {
     article = ["article1", "article2"]
 } else {
     console.log("something went wrong")
 }
 
+//functions
 btn.addEventListener("click", () => {
     title.innerHTML = "Wiktor"
 }
