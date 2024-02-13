5bd8705b28a50211fd98d5bff53d9c1142bf65b2
Wiktor Piela <wpiela9@gmail.com>
2024-02-13 18:42:41 +0100

    adticle retrieve update delete added view and url

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'news'
 urlpatterns = [
     path('article-list-create/', views.ArticleListCreate.as_view(), name='articleListCreate'),
+    path('article-details/<int:pk>/', views.ArticleDetail.as_view(), name='articleDetails'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,17 @@
 from rest_framework import generics, permissions
 from news.models import Article
 from news.serializers import ArticleSerializer
+from .permissions import IsOwnerOrReadOnly
 
 class ArticleListCreate(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [IsOwnerOrReadOnly]
