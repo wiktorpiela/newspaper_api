5e908d5c4b8f0044936598486194fc4adb096af1
Wiktor Piela <wpiela9@gmail.com>
2024-02-27 06:38:41 +0100

    UserSerializer - email uniquevalidator added, create user with or without email, update method - modify existing user

--- news/serializers.py
+++ news/serializers.py
@@ -3,6 +3,7 @@ from news.models import Article
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
+from rest_framework.validators import UniqueValidator
 
 class ArticleSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField()
@@ -13,6 +14,7 @@ class ArticleSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     articles = serializers.StringRelatedField(many=True)
+    email = serializers.EmailField(required=False, validators=[UniqueValidator(queryset=User.objects.all())])
     class Meta:
         model = User
         fields = ("id", "username", "email", "password", "articles",) # pola które widać
@@ -36,5 +38,40 @@ class UserSerializer(serializers.ModelSerializer):
         except ValidationError as e:
             raise exceptions.ValidationError(e)
         else:
+            if email:
+                user = User.objects.create_user(username=username, email=email, password=password)
+            else:
+                user = User.objects.create_user(username=username, password=password)
+            return user
+
+    # tylko owner - permisja do modelu
+    # wszystkie dane (username, haslo) - email opcjonalnie
+    # id usera w endpoincie
+    # create vs. create_user - jesli create to nie bedzie miał zaszyfrowanego hasła w bazie
+    # user.password=password - user.set_password(password) - przekaz stae haslo opcjonalnie
+
+    def update(self, instance, validated_data):
+        username = validated_data.get("username")
+        email = validated_data.get("email")
+        password = validated_data.get("password")
+
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            raise exceptions.ValidationError(e)
+        else:
+            instance.username = username
+            instance.set_password(password)
+            if email:
+                instance.email = email
+            instance.save()
+            return instance
+
+
+
+
+
+
+
 
 
