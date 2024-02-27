6796da7682f63757435ee51377cb02f7932121b2
Wiktor Piela <wpiela9@gmail.com>
2024-02-27 06:32:15 +0100

    format data when displaying on browser in .json format

--- drf_newspaper/urls.py
+++ drf_newspaper/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', views.obtain_auth_token),
     path('', include('news.urls')),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
