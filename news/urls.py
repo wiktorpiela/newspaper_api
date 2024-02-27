1779d2b430256173b9e616c7af4e35395076a181
Wiktor Piela <wpiela9@gmail.com>
2024-02-27 06:43:23 +0100

    both api root view and user details view endpoints added

--- news/urls.py
+++ news/urls.py
@@ -5,5 +5,7 @@ app_name = 'news'
 urlpatterns = [
     path('article-list-create/', views.ArticleListCreate.as_view(), name='articleListCreate'),
     path('article-details/<int:pk>/', views.ArticleDetail.as_view(), name='articleDetails'),
-    path('user-list/', views.UserList.as_view(), name='userList'),
+    path('user-list-create/', views.UserView.as_view(), name='userListCreate'),
+    path('user-details/<int:pk>/', views.UserDetail.as_view(), name='userDetails'),
+    path('', views.APIRootView.as_view(), name='root'),
 ]
