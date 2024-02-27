145c0bfd3d46ce4e82b864ff146a62df2cb66912
Wiktor Piela <wpiela9@gmail.com>
2024-02-27 06:41:10 +0100

    APIRootView added - it returns all endpoins in app, UserDetail generic view to modify user data added with custom permission

--- news/views.py
+++ news/views.py
@@ -1,9 +1,13 @@
 from rest_framework import generics, permissions
 from rest_framework.authtoken.admin import User
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.reverse import reverse
+import random
 
 from news.models import Article
 from news.serializers import ArticleSerializer, UserSerializer
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly, IsUserAccountOwner
 from django.contrib.auth.models import User
 
 class ArticleListCreate(generics.ListCreateAPIView):
@@ -19,6 +23,31 @@ class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ArticleSerializer
     permission_classes = [IsOwnerOrReadOnly]
 
-class UserList(generics.ListAPIView):
+class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        return [permissions.AllowAny() if self.request.method=='POST' else permissions.IsAdminUser()]
+
+class UserDetail(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsUserAccountOwner]
+
+class APIRootView(APIView):
+
+    def get(self, request, format=None):
+        articles = Article.objects.all()
+        pk = random.choice(articles).id
+        links = {
+            'articles': reverse('news:articleListCreate', request=request, format=format),
+            'article-details': reverse('news:articleDetails', args = [pk], request=request, format=format),
+            'users': reverse('news:userListCreate', request=request, format=format),
+        }
+        return Response(links)
+
+
+
+
+
