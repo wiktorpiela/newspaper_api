b4a9ad392adde8e7b8e504153dd2e97005f3b5ea
Wiktor Piela <wpiela9@gmail.com>
2024-03-05 06:54:48 +0100

    userserializer added - related articles and email filed validated - uniquevalidator

--- news/serializers.py
+++ news/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from news.models import Article
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.models import User
 
 class ArticleSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField()
@@ -7,3 +9,9 @@ class ArticleSerializer(serializers.ModelSerializer):
         model = Article
         fields = ('id', 'title', 'text', 'pub_date', 'author')
         read_only_fields = ('author',)
+
+class UserSerializer(serializers.ModelSerializer):
+    articles = serializers.StringRelatedField(many=True)
+    email = serializers.EmailField(required=False, validators=[UniqueValidator(queryset=User.objects.all())])
+    class Meta:
+        model = User
