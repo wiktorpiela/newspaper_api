fcb566a36648d8d7f424b7dfbe389c096aedb0af
Wiktor Piela <wpiela9@gmail.com>
2024-03-05 07:03:40 +0100

    endpoint for UserView added

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('hello-world/', views.HelloWorldView.as_view(), name='hellowWorld'),
     path('article-list-create/', views.ArticleListCreate.as_view(), name='articleListCreate'),
     path('article-details/<int:pk>/', views.ArticleDetail.as_view(), name='articleDetail'),
+    path('user-list-create/', views.UserView.as_view(), name='userListCreate'),
 ]
