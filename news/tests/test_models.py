ca9e85cf3ca7e8e280932094dd2692bdddf9de69
Wiktor Piela <wpiela9@gmail.com>
2024-03-20 19:58:39 +0100

    started with asticle model testing

--- news/tests/test_models.py
+++ news/tests/test_models.py
@@ -2,7 +2,14 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from ..models import Article
 
+#setup - uruchamia przed funkcjami obiekty i intancje potrzebne do testowania
+
 class ArticleTestCase(TestCase):
 
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='122334')
+        self.article = Article.objects.create(title='test title', text='test text', author=self.user)
+
     def test_article(self):
-        self.assertEqual(1, 1)
+        article = Article.objects.get(id=self.article.id)
+        self.assertEqual(article.title, self.article.title)
