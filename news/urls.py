84de40c5baf4acefff1c188e6a4dbcb73260fd51
Wiktor Piela <wpiela9@gmail.com>
2024-04-23 19:14:50 +0200

    router for viewset endpoint added

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('user-viewset', views.UserViewSet)
 
 app_name = 'news'
 urlpatterns = [
@@ -7,5 +11,7 @@ urlpatterns = [
     path('article-details/<int:pk>/', views.ArticleDetail.as_view(), name='articleDetails'),
     path('user-list-create/', views.UserView.as_view(), name='userListCreate'),
     path('user-details/<int:pk>/', views.UserDetail.as_view(), name='userDetails'),
-    path('', views.APIRootView.as_view(), name='root'),
+    
+    #path('', views.APIRootView.as_view(), name='root'),
+    path('', include(router.urls)),
 ]
